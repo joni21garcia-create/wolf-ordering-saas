0567d511605a296c6f8b0412bae5fa925a6f0149
TuNombre <tuemail@gmail.com>
2026-07-16 21:33:06 -0500

    chore: instalar sharp para producción y corregir error de compilación nativa

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
    CORS/Opacos desde el bucket de Supabase para instalabilidad.
 ============================================================ */
 
-const VERSION = "1784229556770";
+const VERSION = "1784255433930";
 
 const CACHE = {
     STATIC: `wolf-static-${VERSION}`,
