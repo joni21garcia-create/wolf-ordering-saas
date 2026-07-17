a81b38401fce9815b4b0c3996df6b3facaa89a45
TuNombre <tuemail@gmail.com>
2026-07-17 07:48:26 -0500

    chore: rebulit and verified local build successfully

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
    CORS/Opacos desde el bucket de Supabase para instalabilidad.
 ============================================================ */
 
-const VERSION = "1784287466753";
+const VERSION = "1784292343470";
 
 const CACHE = {
     STATIC: `wolf-static-${VERSION}`,
