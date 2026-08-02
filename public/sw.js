3e1c337e762cad5fd9b6b8b3f9810d4e11a1ed6c
TuNombre <tuemail@gmail.com>
2026-08-02 07:24:55 -0500

    chore: update service worker version

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
    CORS/Opacos desde el bucket de Supabase para instalabilidad.
 ============================================================ */
 
-const VERSION = "1785659625413";
+const VERSION = "1785673245646";
 
 const CACHE = {
     STATIC: `wolf-static-${VERSION}`,
